42b038622f17e5f39a8dd3482fae5926a907ee0b
Gleison T Loiola <gleisontorres46@gmail.com>
2026-08-28 19:01:10 -0300

    feat: reformula interface da página de perfil

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login', redirect_field_name='next')
 def user_profile(request):
     return render(request, 'user_profile.html')

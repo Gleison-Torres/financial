66f5cb06ac702b1b6a06f78779e3d1e2c9593845
Gleison T Loiola <gleisontorres46@gmail.com>
2026-08-29 03:55:52 -0300

    wip: adiciona rotas para alteração de senha do perfil

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.user_profile, name='profile'),
-    path('edit-profile/', views.edit_profile, name='edit_profile')
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('edit-password/', views.edit_password, name='edit_password')
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -10,3 +10,9 @@ def user_profile(request):
 @login_required(login_url='login', redirect_field_name='next')
 def edit_profile(request):
     return render(request, 'edit_profile.html')
+
+
+@login_required(login_url='login', redirect_field_name='next')
+def edit_password(request):
+    return render(request, 'edit_password.html')
+

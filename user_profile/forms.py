038837a318bebe547a1fca9290dddf8f1a63b936
Gleison T Loiola <gleisontorres46@gmail.com>
2026-09-01 23:38:42 -0300

    feat: implementa alteração de senha pelo perfil

---
+++ user_profile/forms.py
@@ -0,0 +1,47 @@
+from accounts.forms import PasswordValidatorMixin
+from django import forms
+from django.core.validators import ValidationError
+
+
+class ChangePasswordForm(PasswordValidatorMixin, forms.Form):
+
+    current_password = forms.CharField(
+        widget=forms.PasswordInput,
+        strip=False
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput,
+        strip=False
+    )
+
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput,
+        strip=False
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = user
+
+    def clean_current_password(self):
+        current = self.cleaned_data.get('current_password')
+
+        if not self.user.check_password(current):
+            raise ValidationError('Senha atual incorreta.')
+
+        return current
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        confirm = cleaned_data.get('confirm_password')
+
+        if password and confirm and password != confirm:
+            raise ValidationError({
+                'password': 'As senhas não coincidem.',
+                'confirm_password': 'As senhas não coincidem.'
+            })
+
+        return cleaned_data
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from . forms import ChangePasswordForm
+from django.contrib import messages
 
 
 @login_required(login_url='login', redirect_field_name='next')
@@ -9,10 +12,29 @@ def user_profile(request):
 
 @login_required(login_url='login', redirect_field_name='next')
 def edit_profile(request):
+    if request.method == 'POST':
+        form = request.POST
+        print(form.get('fullName'), form.get('email'), form.get('username'))
     return render(request, 'edit_profile.html')
 
 
 @login_required(login_url='login', redirect_field_name='next')
 def edit_password(request):
-    return render(request, 'edit_password.html')
+    if request.method == 'POST':
+        form = ChangePasswordForm(request.user, request.POST)
+
+        if form.is_valid():
+            request.user.set_password(form.cleaned_data['password'])
+            request.user.save()
+
+            update_session_auth_hash(request, request.user)
+
+            messages.success(request, 'Senha alterada com sucesso.')
+            return redirect('profile')
+
+    else:
+        form = ChangePasswordForm(request.user)
+
+    return render(request,'edit_password.html', {'form': form})
+
 

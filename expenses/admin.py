c3c1cf7eac4a8dd011cfb09bd7d975bed558332c
Gleison T Loiola <gleisontorres46@gmail.com>
2026-09-17 01:00:08 -0300

    feat: configura visualização de despesas no admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'user',
+        'amount',
+        'date',
+        'category',
+        'installment',
+        'installment_group',
+    )
+
+    list_filter = (
+        'category',
+        'date',
+    )
+
+    search_fields = (
+        'title',
+        'user__username',
+        'user__email',
+    )
+
+    ordering = (
+        '-date',
+    )
+
+    @admin.display(description='Parcela')
+    def installment(self, obj):
+        if obj.installment_number and obj.total_installments:
+            return f'{obj.installment_number}/{obj.total_installments}'
+
+        return '-'

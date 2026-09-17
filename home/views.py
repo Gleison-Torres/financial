4dc63cac0b58f5a3c74d1ee0ef92f2595e13cb5b
Gleison T Loiola <gleisontorres46@gmail.com>
2026-09-17 00:53:06 -0300

    fix: corrige exibição do nome do usuário na home

--- home/views.py
+++ home/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login', redirect_field_name='next')
 def home(request):
-    first_name = request.user.first_name.split()[0]
-    return render(request, 'home.html', {'first_name': first_name})
 
+    name_parts = request.user.first_name.split()
+
+    first_name = (
+        name_parts[0]
+        if name_parts
+        else request.user.username
+    )
+
+    return render(request, 'home.html', {'first_name': first_name})
